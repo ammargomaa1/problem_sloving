cc4850c912f8d85e010db20b51ccad37814ef7f9
ammargomaa1 <ammargomaa1@gmail.com>
2022-04-28 01:58:21 +0200

    rotate

---
+++ Rotate Array/Rotate_Array.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return NULL
+     */
+    function rotate(&$nums, $k) {
+        
+        if($k > count($nums)) $k = $k -count($nums);
+
+        $arr1 = array_slice($nums,count($nums)-$k,$k);
+       
+
+        $arr2 = array_slice($nums,0,count($nums)-$k);
+
+        $nums = array_merge($arr1,$arr2);
+        return $nums;
+    }
+}
+
+$r = [3,5,6];
+$n = new Solution;
+
+var_dump($n->rotate($r,4));
